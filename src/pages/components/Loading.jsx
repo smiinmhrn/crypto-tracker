d66ccea506fd7e12cc59fb4e19ad3d7587f1fc88
Samin <samin.mhr2004@gmail.com>
2026-08-14 12:15:02 +0330

    created loading component.

--- src/pages/components/Loading.jsx
+++ src/pages/components/Loading.jsx
@@ -1,3 +1,10 @@
-const Loading = () => {};
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="spinner" />
+      <p>loading crypto data...</p>
+    </div>
+  );
+};
 
 export default Loading;
